1711012e2c58f92f6b61cb8c66587c4707226b70
ttahb <bhattvijay69@hotmail.com>
2024-03-06 02:08:42 +0530

    added few models

---
+++ models/Product.model.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const User = require("./User.model");
+
+const productSchema = new Schema(
+    {
+       userId: {
+        type: Schema.Types.ObjectId,
+        ref: User
+       }, 
+
+       productName: {
+        type: String,
+        required: true,
+       },
+
+       price: {
+        type: Number,
+        required:true 
+       }, 
+
+       description: {
+        type: String,
+        required: true
+       },
+
+       image: {
+        type: String,
+        default: '' //TODO AIM - Add a default image later.
+       },
+
+       hearts: {
+        type: Number
+       },
+
+       tags: {
+        type: String,
+        enum: ['wine', 'jams', 'chocolates', 'cookies', 'cakes']  // TODO  AIM - Add appropriate tags here.
+       }
+    },
+    {
+      // this second object adds extra properties: `createdAt` and `updatedAt`    
+      timestamps: true
+    }
+  );
+  
+  const Product = model("Product", productSchema);
+  
+  module.exports = User;
--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,10 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    fullName: {
+      type: String,
+      required: true
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
@@ -10,10 +14,23 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true
     },
+    role: {
+      type: String,
+      enum: ['admin', 'customer'],
+      default: 'customer'
+    },
+    companyName: {
+      type: String,
+    },
+    companySize: {
+      type: String,
+      enum: ['0-100', '101-1000', '1001-10000', '10000+']
+    },
     password: {
       type: String,
       required: [true, 'Password is required.']
-    }
+    },
+    
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
