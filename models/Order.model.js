f00d08fe003860a82f06ae7522e4ec26f73a8915
Pencho Ivanov <penchoivanov@Penchos-MacBook-Air.local>
2024-03-06 11:34:13 +0100

    models done-ish

--- models/Order.model.js
+++ models/Order.model.js
@@ -18,10 +18,33 @@ const orderSchema = new Schema(
         status: {
             type: String,
             // https://support.bigcommerce.com/s/article/Order-Statuses?language=en_US
-            // Sorce I found interesting to consider more the 
+            // Sorce I found interesting to consider more then the 3 fileds.
             enum: ["Pending", "Initiated", "Awaiting Payment", "In-progress", "Shipped", "Delivered", "Cancelled", "Partially Refunded", "Refunded"],
             default: "Pending",
         }, 
+        content: [{
+            productId:{
+                type: String,
+                require: true
+            },
+            productName: {
+                type: String,
+                require: true
+            },
+        // Suggestion for the top 2 fileds, to refer to the obj.Type Product
+                // product: {
+                //     type: Schema.Types.ObjectId,
+                //     ref: "Product"
+                // }
+            price: {
+                type:Number,
+                require: true
+            },
+            quantity: {
+                type: Number,
+                require: true
+            }
+        }]
     },
     {
         timestamps: true
