6cb3e40cb0261f99872e923c640fdd91f099dff6
ttahb <bhattvijay69@hotmail.com>
2024-03-25 20:31:44 +0530

    stripe integration - positive scenario success

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use("/api", isAuthenticated, orderRouter);
 const userRoutes = require("./routes/user.routes");
 app.use("/api", isAuthenticated, userRoutes);
 
+const paymentRoutes = require('./routes/payments.routes');
+app.use('/api', paymentRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
